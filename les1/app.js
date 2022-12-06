2ae026184e1db1a5e7f0473e90b5f69ccfea3acd
student <student@lt20421.media.local>
2022-12-06 12:13:08 +0100

    we hebben javascript gemaakt

---
+++ les1/app.js
@@ -0,0 +1,24 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world");
+        let appNaam = "Facebook"
+         console.log(appNaam) ;
+        let versienummer = 0.9
+         console.log(versienummer) ; 
+         let versiedatum = "6 december";
+         console.log(versiedatum) ;
+         let autheur = "omar";
+         console.log(autheur)
+         let copyright = "true";
+         console.log(copyright) 
+         let distributeur = "";
+         console.log(distributeur)
+         let = darkmode = 5<4;
+         console.log(darkmode)
+    }
+}
+
+let app = new App();
+app.runApplication();
